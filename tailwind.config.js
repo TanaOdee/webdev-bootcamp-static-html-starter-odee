cc4fba6874bcb0145a45846be3bfbebe0e6d5f69
tanapat odee <vechasilp@gmail.com>
2023-12-23 15:20:36 +0700

    Update Text Divine

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // primary: "#d8eaff",
         primary: "#DEF81D",
         "wd-white": "#FFFFFF",
         "wd-grey": "#999999",
